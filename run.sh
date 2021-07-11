d894b2f336a4f5a0803a947160d758cd786fef6f
Antwan <antwanmeave@tutanota.com>
2021-07-11 18:23:12 -0500

    changed server into foreground so server can be manually interacted with

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-bin/x64/factorio --start-server-load-latest --server-settings data/server-settings.json --server-adminlist server-adminlist.json --console-log log.txt & ./log_file_parser.py && fg
+./log_file_parser.py & bin/x64/factorio --start-server-load-latest --server-settings data/server-settings.json --server-adminlist server-adminlist.json --console-log log.txt && fg
